72385b15f8e53b63c2bf98d9534250ecda248e79
Jakub Jankiewicz <jcubic@onet.pl>
2018-05-27 20:26:18 +0200

    Add Git Web Terminal to list of awesome users (close #9) (PR #11)

--- website/users.js
+++ website/users.js
@@ -5,6 +5,12 @@ const users = [
     image: '/img/usedby/nde.png',
     infoLink: 'https://github.com/wmhilton/nde',
     pinned: true
+  },
+  {
+    caption: 'GIT Web Terminal',
+    image: '/img/usedby/git-web-terminal.png',
+    infoLink: 'https://github.com/jcubic/git',
+    pinned: true
   }
 ]
 module.exports = users
