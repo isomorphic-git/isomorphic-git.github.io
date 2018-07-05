488bc7881db626ae48b75d0db6b63a01ab34c370
William Hilton <wmhilton@gmail.com>
2018-07-05 16:41:42 -0400

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -51,7 +51,7 @@ const siteConfig = {
     theme: 'default'
   },
   scripts: [
-    'https://buttons.github.io/buttons.js',
+    //'https://buttons.github.io/buttons.js',
     '/js/gitter.js',
     '/js/sidecar.v1.js',
     '/js/browserfs.js',
