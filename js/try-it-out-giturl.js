03225c1e1f7244e76feb034e2fdf782b872250e6
William Hilton <wmhilton@gmail.com>
2020-02-25 21:51:29 -0500

    chore: fix the docs website (#1062)

--- js/try-it-out-giturl.js
+++ js/try-it-out-giturl.js
@@ -1,5 +1,8 @@
+import http from './isomorphic-git/http/web/index.js'
+
 document.addEventListener('DOMContentLoaded', function listener () {
   document.removeEventListener('DOMContentLoaded', listener)
+  let $output = document.getElementById('try-it-output')
   let $input = document.getElementById('giturl_input')
   let $button = document.getElementById('giturl_button')
   const change = async function change (event) {
@@ -7,9 +10,11 @@ document.addEventListener('DOMContentLoaded', function listener () {
     value = value.replace(/^https?:\/\//, '')
     if (!value.endsWith('.git')) value += '.git'
     let info = await git.getRemoteInfo({
-      url: `https://cors.isomorphic-git.org/${value}`
+      http,
+      url: value,
+      corsProxy: 'https://cors.isomorphic-git.org'
     })
-    const limit = 100;
+    const limit = 1000;
     if (info.refs.tags) {
       let $tags = document.getElementById('giturl_tags')
       let tagstext = Object.keys(info.refs.tags).join(', ')
@@ -20,6 +25,7 @@ document.addEventListener('DOMContentLoaded', function listener () {
       let branchestext = Object.keys(info.refs.heads).join(', ')
       $branches.innerText = branchestext.slice(0, limit)
     }
+    $output.classList.add('show')
   }
   if ($input && $button) {
     $input.addEventListener('change', change)
