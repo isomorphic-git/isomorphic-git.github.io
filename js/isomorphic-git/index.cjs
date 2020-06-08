597348f9fd70884749978f6d424572bdb0e5ea95
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2020-06-08 04:48:59 +0200

    perf(push): don't send any objects if commit is already on remote (#1137)

--- js/isomorphic-git/index.cjs
+++ js/isomorphic-git/index.cjs
@@ -10506,13 +10506,16 @@ async function _push({
       }
     }
 
-    const commits = await listCommitsAndTags({
-      fs,
-      gitdir,
-      start: [oid],
-      finish,
-    });
-    objects = await listObjects({ fs, gitdir, oids: commits });
+    // If remote does not have the commit, figure out the objects to send
+    if (!finish.includes(oid)) {
+      const commits = await listCommitsAndTags({
+        fs,
+        gitdir,
+        start: [oid],
+        finish,
+      });
+      objects = await listObjects({ fs, gitdir, oids: commits });
+    }
 
     if (thinPack) {
       // If there's a default branch for the remote lets skip those objects too.
