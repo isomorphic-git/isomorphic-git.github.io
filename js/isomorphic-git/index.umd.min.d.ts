f9417ece2ac821efc8f1b47fe665f3966ef6d7ed
Ray Foss <ray@vblip.com>
2022-08-17 00:50:22 +0900

    Handle edge cases in walk diff example (#1622)
    * Handle edge cases in walk diff example
    
    * Update walk.js
    
    Remove irrelevant code

--- js/isomorphic-git/index.umd.min.d.ts
+++ js/isomorphic-git/index.umd.min.d.ts
@@ -3415,12 +3415,14 @@ export function version(): string;
  *
  * Example 2: Return the difference between the working directory and the HEAD commit
  * ```js
- * const diff = require('diff-lines')
- * async function map(filepath, [head, workdir]) {
+ * const map = async (filepath, [head, workdir]) => {
  *   return {
  *     filepath,
- *     oid: await head.oid(),
- *     diff: diff((await head.content()).toString('utf8'), (await workdir.content()).toString('utf8'))
+ *     oid: await head?.oid(),
+ *     diff: diff(
+ *       (await head?.content())?.toString('utf8') || '',
+ *       (await workdir?.content())?.toString('utf8') || ''
+ *     )
  *   }
  * }
  * ```
