33977f061d4dd6c977e0bc08615dadd00d2dd6d8
Sebastien <colladonsebastien@gmail.com>
2024-01-07 21:04:21 +0100

    fix: `normalizePath` performance (#1853)
    * docs: add @scolladon as a contributor
    
    * feat: improve `normalizePath` performance
    
    * feat: memoize normalizePath
    
    * refactor: improve readability

--- js/isomorphic-git/index.js
+++ js/isomorphic-git/index.js
@@ -1442,16 +1442,32 @@ function compareRefNames(a, b) {
   return tmp
 }
 
+const memo = new Map();
 function normalizePath(path) {
+  let normalizedPath = memo.get(path);
+  if (!normalizedPath) {
+    normalizedPath = normalizePathInternal(path);
+    memo.set(path, normalizedPath);
+  }
+  return normalizedPath
+}
+
+function normalizePathInternal(path) {
+  path = path
+    .split('/./')
+    .join('/') // Replace '/./' with '/'
+    .replace(/\/{2,}/g, '/'); // Replace consecutive '/'
+
+  if (path === '/.') return '/' // if path === '/.' return '/'
+  if (path === './') return '.' // if path === './' return '.'
+
+  if (path.startsWith('./')) path = path.slice(2); // Remove leading './'
+  if (path.endsWith('/.')) path = path.slice(0, -2); // Remove trailing '/.'
+  if (path.length > 1 && path.endsWith('/')) path = path.slice(0, -1); // Remove trailing '/'
+
+  if (path === '') return '.' // if path === '' return '.'
+
   return path
-    .replace(/\/\.\//g, '/') // Replace '/./' with '/'
-    .replace(/\/{2,}/g, '/') // Replace consecutive '/'
-    .replace(/^\/\.$/, '/') // if path === '/.' return '/'
-    .replace(/^\.\/$/, '.') // if path === './' return '.'
-    .replace(/^\.\//, '') // Remove leading './'
-    .replace(/\/\.$/, '') // Remove trailing '/.'
-    .replace(/(.+)\/$/, '$1') // Remove trailing '/'
-    .replace(/^$/, '.') // if path === '' return '.'
 }
 
 // For some reason path.posix.join is undefined in webpack
